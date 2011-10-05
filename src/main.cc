724ea4a0cdeab99dd42cebd5964c579bbd2899ca
Chris Rorvick <chris.rorvick@gmail.com>
2011-10-04 22:04:10 -0500

    Moved to newer autotools conventions.

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,11 @@
 
+#include <config.h>
+
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
     std::cout << "Hello, world!" << std::endl;
+    std::cout << "This is " PACKAGE_STRING "." << std::endl;
 }
 
